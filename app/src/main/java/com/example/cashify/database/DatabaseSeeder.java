4040a82e937d3213ea1b4588ee52111c7c345c2e
trucanh <24520085@gm.uit.edu.vn>
2026-03-29 13:24:57 +0700

    [Fix] AddTransaction

--- app/src/main/java/com/example/cashify/database/DatabaseSeeder.java
+++ app/src/main/java/com/example/cashify/database/DatabaseSeeder.java
@@ -1,6 +1,9 @@
 package com.example.cashify.database;
 
 import android.content.Context;
+
+import com.example.cashify.R;
+
 import java.util.concurrent.Executors;
 
 public class DatabaseSeeder {
@@ -13,29 +16,29 @@ public class DatabaseSeeder {
             if(dao.getCategoriesByType(0).isEmpty()&&dao.getCategoriesByType(1).isEmpty()){
                 //Danh sách này chứa các danh mục phổ biến
                 //--thu vao--//
-                dao.insert(makeCategory("Lương", "ic_salary", "#000000", 1, 1));
-                dao.insert(makeCategory("Gia đình cho", "ic_family", "#000000", 1, 1));
-                dao.insert(makeCategory("Thưởng", "ic_bonus", "#000000", 1, 1));
+                dao.insert(makeCategory("Lương", "ic_salary", R.color.black, 1, 1));
+                dao.insert(makeCategory("Gia đình cho", "ic_family", R.color.black, 1, 1));
+                dao.insert(makeCategory("Thưởng", "ic_bonus", R.color.black, 1, 1));
 
                 //--chi ra--//
-                dao.insert(makeCategory("Ăn uống", "ic_food", "#000000", 0, 1));
-                dao.insert(makeCategory("Di chuyển", "ic_transport", "#000000", 0, 1));
-                dao.insert(makeCategory("Mua sắm", "ic_shopping", "#000000", 0, 1));
-                dao.insert(makeCategory("Hóa đơn", "ic_bill", "#000000", 0, 1));
-                dao.insert(makeCategory("Cafe", "ic_cafe", "#000000", 0, 1));
-                dao.insert(makeCategory("Giải trí", "ic_entertain", "#000000", 0, 1));
-                dao.insert(makeCategory("Xăng xe", "ic_fuel", "#000000", 0, 1));
-                dao.insert(makeCategory("Tiền trọ", "ic_house", "#000000", 0, 1));
-                dao.insert(makeCategory("Khác", "ic_other", "#000000", 0, 1));
+                dao.insert(makeCategory("Ăn uống", "ic_food", R.color.black, 0, 1));
+                dao.insert(makeCategory("Di chuyển", "ic_transport", R.color.black, 0, 1));
+                dao.insert(makeCategory("Mua sắm", "ic_shopping", R.color.black, 0, 1));
+                dao.insert(makeCategory("Hóa đơn", "ic_bill", R.color.black, 0, 1));
+                dao.insert(makeCategory("Cafe", "ic_cafe", R.color.black, 0, 1));
+                dao.insert(makeCategory("Giải trí", "ic_entertain", R.color.black, 0, 1));
+                dao.insert(makeCategory("Xăng xe", "ic_fuel", R.color.black, 0, 1));
+                dao.insert(makeCategory("Tiền trọ", "ic_house", R.color.black, 0, 1));
+                dao.insert(makeCategory("Khác", "ic_other", R.color.black, 0, 1));
 
             }
         });
     }
-    private static Category makeCategory(String name, String icon, String color, int type, int isDefault){
+    private static Category makeCategory(String name, String icon, int colorResId, int type, int isDefault){
         Category c=new Category();
         c.name=name;
         c.iconName=icon;
-        c.colorCode=color;
+        c.colorCode = String.valueOf(colorResId);
         c.type=type;
         c.isDefault=isDefault;
         c.isDeleted=0;
